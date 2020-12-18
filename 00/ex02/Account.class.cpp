cc61d567089d58ae0eb51eba0bb013d325e167bd
MakhmudEgo <MakhmudEgo@yandex.ru>
2020-12-18 21:31:52 +0300

    upt .gitignore

---
+++ 00/ex02/Account.class.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Mahmud Jego on 12/18/20.
+//
+
+#include "Account.class.hpp"
+
+int Account::getNbAccounts(void) {
+	return 0;
+}
+
+int Account::getTotalAmount(void) {
+	return 0;
+}
+
+int Account::getNbDeposits(void) {
+	return 0;
+}
+
+int Account::getNbWithdrawals(void) {
+	return 0;
+}
+
+void Account::displayAccountsInfos(void) {
+
+}
+
+Account::Account(int initial_deposit) {
+
+}
+
+Account::~Account(void) {
+
+}
+
+void Account::makeDeposit(int deposit) {
+
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+	return false;
+}
+
+int Account::checkAmount(void) const {
+	return 0;
+}
+
+void Account::displayStatus(void) const {
+
+}
+
+void Account::_displayTimestamp(void) {
+
+}
+
+Account::Account(void) {
+
+}
+
