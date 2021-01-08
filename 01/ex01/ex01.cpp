78da54ee577b6c63bde5547f2251d9c879f7a60f
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-01-08 20:21:07 +0300

    ok day 01 ex 01

---
+++ 01/ex01/ex01.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Mahmud Jego on 1/8/21.
+//
+
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+
+	std::cout << *panther << std::endl;
+	delete panther;
+}
+
+int main()
+{
+	memoryLeak();
+	return (0);
+}
